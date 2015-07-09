6a8aa831643e99829c09f6303a3101322b05d584
lanou3g <k1419488421@163.com>
2015-07-09 22:25:06 +0800

    no message

---
+++ 小康新闻/model/TopLine/NormalNews/News.h
@@ -0,0 +1,13 @@
+//
+//  News.h
+//  SCNavTabbarDemo
+//
+//  Created by lanou3g on 15/7/9.
+//  Copyright (c) 2015年 SCNavTabbarDemo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface News : NSObject
+
+@end
---
+++ 小康新闻/model/TopLine/PicNews/PicNews.h
@@ -0,0 +1,13 @@
+//
+//  PicNews.h
+//  SCNavTabbarDemo
+//
+//  Created by lanou3g on 15/7/9.
+//  Copyright (c) 2015年 SCNavTabbarDemo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PicNews : NSObject
+
+@end
